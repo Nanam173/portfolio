ab34eacc43054613fabcab17b66d53d27b3cd134
nanam173 <nnm.hori@gmail.com>
2025-05-23 22:06:47 +1000

    'get' -> 'resources' in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "contacts/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :contacts, only: :index
 end
