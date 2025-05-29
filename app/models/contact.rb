de86cec8edb77704fd20872c192d251543ce853b
nanam173 <nnm.hori@gmail.com>
2025-05-29 22:52:37 +1000

    add annotation to code comment

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  url        :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Contact < ApplicationRecord
     validates :name, presence: true
 
