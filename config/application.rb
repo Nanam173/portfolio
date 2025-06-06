ee84ed002d9b78a76f14331f3379e04898375ac7
nanam173 <nnm.hori@gmail.com>
2025-06-06 22:11:17 +1000

    Revert "elete action cable func"
    This reverts commit c8b4a58fa96c2a89f2c982142bb88a335dc130df.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module Portfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.action_cable.mount_path = nil
   end
 end
